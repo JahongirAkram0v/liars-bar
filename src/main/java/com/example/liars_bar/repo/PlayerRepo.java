683306a722651e93326f8283d1ddd9820a90f678
JahongirAkram0v <jahongirakramov0000@gmail.com>
2025-11-17 09:50:19 +0500

    u bu ish qilgim kelsa qilaman

--- src/main/java/com/example/liars_bar/repo/PlayerRepo.java
+++ src/main/java/com/example/liars_bar/repo/PlayerRepo.java
@@ -2,9 +2,6 @@ package com.example.liars_bar.repo;
 
 import com.example.liars_bar.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface PlayerRepo extends JpaRepository<Player, Long> {
 }
--- src/main/java/com/example/liars_bar/service/PlayerService.java
+++ src/main/java/com/example/liars_bar/service/PlayerService.java
@@ -1,6 +1,5 @@
 package com.example.liars_bar.service;
 
-import com.example.liars_bar.model.Group;
 import com.example.liars_bar.model.Player;
 import com.example.liars_bar.repo.PlayerRepo;
 import lombok.RequiredArgsConstructor;
