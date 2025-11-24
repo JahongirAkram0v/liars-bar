c97e5157cf83c73d24a1befb2a8fe45b408fb767
JahongirAkram0v <jahongirakramov0000@gmail.com>
2025-11-24 11:31:47 +0500

    set va deleteni sozladim

--- delete_webhook.sh
+++ delete_webhook.sh
@@ -2,4 +2,5 @@
 # shellcheck disable=SC2046
 export $(grep -v '^#' .env | xargs)
 
-curl -s -X POST "$TELEGRAM_BASE_URL$TELEGRAM_BOT_TOKEN$TELEGRAM_BOT_WEBHOOK_PATH"
+curl -s -X POST "$TELEGRAM_BASE_URL$TELEGRAM_BOT_TOKEN/deleteWebhook"
+echo
--- set_webhook.sh
+++ set_webhook.sh
@@ -3,3 +3,4 @@
 export $(grep -v '^#' .env | xargs)
 
 curl -X POST "$TELEGRAM_BASE_URL$TELEGRAM_BOT_TOKEN/setWebhook" -d "url=$WEBHOOK_URL$TELEGRAM_BOT_WEBHOOK_PATH"
+echo
